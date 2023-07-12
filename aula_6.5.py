fb40f7c0b0082b601dc3bc24c51a21e2d7a2dc06
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 16:14:27 -0300

    aula_6.5.py

--- aula_6.5.py
+++ aula_6.5.py
@@ -1,4 +1,12 @@
-# EXERCÍCIO 5
+''' 
+EXERCÍCIO 5
+Tente criar uma calculadora com funções básicas de soma,
+multiplicação, subtração e divisão. Nessa calculadora você
+deverá utilizar try-except. Caso queira, poderá adicionar mais
+funcionalidades, fique livre para pesquisar funções e adicioná-las
+a sua calculadora.
+
+'''
 
 try:
     x = float(input("Digite o primeiro valor: "))
