e2551510338048f6361943421f2a6a833898f55a
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 15:30:02 -0300

    aula_5.1.py

--- aula_5.1.py
+++ aula_5.1.py
@@ -1,4 +1,9 @@
-# EXERCÍCIO 1
+''' 
+EXERCÍCIO 1
+Elabore um programa que leia um número inteiro qualquer e
+mostre na tela a sua tabuada.
+
+'''
 
 x = int(input("Digite um número: "))
 print("TABUADA DO",x)
