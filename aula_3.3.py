6f548cca64ea8e7387053cec59e602b788292dc4
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 15:20:21 -0300

    aula_3.3.py

--- aula_3.3.py
+++ aula_3.3.py
@@ -1,4 +1,9 @@
-# EXERCÍCIO 3
+''' 
+EXERCÍCIO 3
+Crie um programa que receba 1 número e mostre ao usuário qual
+seu dobro, triplo e raiz quadrada.
+
+'''
 
 num = float(input('Digite um número: '))
 print('Dobro:',num*2)
