4326d6b829944620807d81063c2fae470daa909f
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 20:57:21 -0300

    aula_7.1.py

--- aula_7.1.py
+++ aula_7.1.py
@@ -1,9 +1,17 @@
-# EXERCÍCIO 1
+''' 
+EXERCÍCIO 1
+Desenvolva um programa que leia seis números inteiros e mostre
+a soma apenasdaqueles que forem pares. Se o valor digitado for
+ímpar, desconsidere-o. (Obs: Utilize try except)
 
-total = 0
-print("*** CALCULADORA DE NÚMEROS PARES ***\n")
-for i in range(6):
-    num = float(input("Digite o {}º número: ".format(i+1)))
-    if(num%2==0):
-        total = total + num
-print("\n*** RESULTADO ***\n\n{}".format(total))
+'''
+try:
+    total = 0
+    print("*** CALCULADORA DE NÚMEROS PARES ***\n")
+    for i in range(6):
+        num = float(input("Digite o {}º número: ".format(i+1)))
+        if(num%2==0):
+            total = total + num
+    print("\n*** RESULTADO ***\n\n{}".format(total))
+except:
+    print("*** VALOR INVÁLIDO! ***")
