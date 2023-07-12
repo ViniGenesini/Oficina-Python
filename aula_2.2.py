476c4655f5941d883e922b9642529ad7d37a5072
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 14:53:03 -0300

    aula_2.2.py

--- aula_2.2.py
+++ aula_2.2.py
@@ -1,4 +1,10 @@
-# EXERCÍCIO 2
+''' 
+EXERCÍCIO 2
+Faça um programa que receba 2 números e escolha uma operação
+para ser efetuada. Soma, subtração ou multiplicação e mostre o
+resultado.
+
+'''
 
 num1 = int(input("Número 1: "))
 num2 = int(input("Número 2: "))
