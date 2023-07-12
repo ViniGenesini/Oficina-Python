46010ab9d4be7bd50626e2f6007603bcdfe799a7
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 15:26:02 -0300

    aula_3.7.py

--- aula_3.7.py
+++ aula_3.7.py
@@ -1,4 +1,10 @@
-# EXERCÍCIO 7
+''' 
+EXERCÍCIO 7
+Elabore um programa que solicite ao usuário a distância de uma
+viagem em Km. Calcule o preço da passagem, cobrando R$ 0.20 por
+Km para viagens de até 100 km e R$ 0.15 para viagens mais longas.
+
+'''
 
 km = float(input('Digite a distância da viagem em km: '))
 if(km<=100):
