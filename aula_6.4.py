9726131c1a3106fb9d427c66cb73b2a4dfb4c85e
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 16:13:17 -0300

    aula_6.4.py

--- aula_6.4.py
+++ aula_6.4.py
@@ -1,4 +1,19 @@
-# EXERCÍCIO 4
+''' 
+EXERCÍCIO 4
+Uma fruteira está vendendo frutas com a seguinte tabela de
+preços:
+
+Fruta       Até 3 Kg            Acima de 6 Kg
+Morango     R$ 3,50 por Kg      R$ 4,20 por Kg
+Maçã        R$ 2,80 por Kg      R$ 2,50 por Kg
+
+Se o cliente comprar mais de 12 Kg em frutas ou o valor total da
+compra ultrapassar R$ 35,00, receberá ainda um desconto de
+15% sobre este total. Escreva um algoritmo para ler a
+quantidade (em Kg) de morangos e a quantidade (em Kg) de
+maças adquiridas e escreva o valor a ser pago pelo cliente.
+
+'''
 
 kg_mor = float(input("Digite a quantidade de morangos adquiridos em kg: "))
 kg_mac = float(input("Digite a quantidade de maçãs adquiridas em kg: "))
