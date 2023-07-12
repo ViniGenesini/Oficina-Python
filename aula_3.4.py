4089bbf5c8effd1e6e264142a9d5de7f21c7f4f1
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 15:22:06 -0300

    aula_3.4.py

--- aula_3.4.py
+++ aula_3.4.py
@@ -1,4 +1,9 @@
-# EXERCÍCIO 4
+''' 
+EXERCÍCIO 4
+Crie um programa que leia o preço de um produto e mostre seu
+novo preço após sofrer um reajuste de 8%.
+
+'''
 
 valor = float(input('Digite o valor do produto: '))
 print("Valor reajustado:",valor+(valor*8/100))
