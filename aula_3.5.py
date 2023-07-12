a4a71e20f6c6486d22451fd285aa9fbf45c8e004
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 15:23:32 -0300

    aula_3.5.py

--- aula_3.5.py
+++ aula_3.5.py
@@ -1,4 +1,10 @@
-# EXERCÍCIO 5
+''' 
+EXERCÍCIO 5
+Faça um programa que receba do usuário, as suas horas
+trabalhadas e valor que recebe por hora, mostrando por fim, qual será
+seu salário.
+
+'''
 
 valor = float(input('Digite o valor recebido por hora: R$ '))
 horas = float(input('Digite as horas trabalhadas: '))
