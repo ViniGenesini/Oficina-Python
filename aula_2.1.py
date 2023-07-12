5b4b9a10e35041b92092a31304fe3b777b3860fa
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 14:50:37 -0300

    aula_2.1.py

--- aula_2.1.py
+++ aula_2.1.py
@@ -1,4 +1,9 @@
-# EXERCÍCIO 1
+''' 
+EXERCÍCIO 1
+Faça um programa que receba o nome do usuário e mostre uma
+mensagem de boas-vindas, junto ao nome da pessoa.
+
+'''
 
 nome = input("Nome do usuário: ")
-print("Seja bem-vindo",nome,"!")
+print("Seja bem-vindo(a)",nome,"!")
