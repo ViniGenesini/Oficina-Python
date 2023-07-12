86e05c2e1b82f28390330bd2649e5f99940860ca
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 15:34:54 -0300

    aula_5.4.py

--- aula_5.4.py
+++ aula_5.4.py
@@ -1,4 +1,18 @@
-# EXERCÍCIO 4
+''' 
+EXERCÍCIO 4
+Um clube de natação estrangeiro irá visitar o brasil para avaliar os
+novos atletas e precisa de um programa que leia o ano de nascimento
+de um atleta e mostre sua categoria, de acordo com a idade:
+
+Até 9 anos: MIRIM
+Até 14 anos: INFANTIL
+Até 19 anos: JUNIOR
+Até 25 anos: SÊNIOR
+Acima: MASTER
+
+Faça o programa de acordo com as diretrizes acima.
+
+'''
 
 ano = int(input("Digite seu ano de nascimento: "))
 idade = 2023-ano
