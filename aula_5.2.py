9e0170764f7c2b91da2f2f98f852222dcad372ab
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 15:31:51 -0300

    aula_5.2.py

--- aula_5.2.py
+++ aula_5.2.py
@@ -1,4 +1,12 @@
-# EXERCÍCIO 2
+''' 
+EXERCÍCIO 2
+Escreva um programa que leia a velocidade de um carro. Se ele
+ultrapassar 70 km/h, mostre uma mensagem dizendo que ele foi
+multado e o valor que deverá pagar, caso ele esteja dentro do limite
+permitido, mostre uma mensagem o parabenizando. A multa vai
+custar R$ 10,00 por cada km acima dos 70.
+
+'''
 
 km = int(input("Digite a velocidade do carro em km/h: "))
 x = float(100+10*(km-70))
