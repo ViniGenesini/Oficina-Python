1f129fe6fef34e9315d81b7efa84fb0a2fb447ea
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 15:14:35 -0300

    aula_2.5.py

--- aula_2.5.py
+++ aula_2.5.py
@@ -1,4 +1,15 @@
-# EXERCÍCIO 5
+''' 
+EXERCÍCIO 5
+Faça um programa que receba um número em horas e converta em
+minutos, informando ao usuário o horário digitado e os minutos ao qual
+foi convertido.
 
-horas = int(input("Insira horas: "))
-print("Horas em min:", horas*60)
+ATUALIZADO EM: 12/07/2023
+
+'''
+
+horas = float(input("Insira horas: "))
+horas_inteiras = int(horas)
+minutos = (horas_inteiras-horas)*-60
+print(f"Horário: {horas_inteiras}:{int(minutos)}")
+print(f"Minutos: {horas*60}")
