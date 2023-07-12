4b97a88c57d32a9a0f17c300e45d2d2e7597d713
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 14:56:22 -0300

    aula_2.4.py

--- aula_2.4.py
+++ aula_2.4.py
@@ -1,4 +1,9 @@
-# EXERCÍCIO 4
+''' 
+EXERCÍCIO 4
+Crie um programa que converta um valor inteiro de centímetros em
+metros.
+
+'''
 
 cm = float(input("Insira cm: "))
 print("Cm em metros:", cm/100)
