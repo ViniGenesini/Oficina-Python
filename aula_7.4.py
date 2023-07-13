85b0fa232d5186e7c903880f5981149ea9df7297
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 21:23:33 -0300

    aula_7.4.py

--- aula_7.4.py
+++ aula_7.4.py
@@ -1,4 +1,17 @@
-# EXERCÍCIO 4
+''' 
+EXERCÍCIO 4
+Faça um Programa que leia um número inteiro menor que 1000
+e imprima a quantidade de centenas, dezenas e unidades dele.
+Observando os termos no plural a colocação do "e", da vírgula
+entre outros. Exemplo:
+
+326 = 3 centenas, 2 dezenas e 6 unidades
+12 = 1 dezena e 2 unidades
+
+Testar com: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 
+25, 20, 10, 21, 11, 1, 7 e 16.
+
+'''
 
 print("*** CALCULADORA DE CASAS DECIMAIS ***\n")
 try:
@@ -11,20 +24,20 @@ try:
         U = int(num-(C*100)-(D*10))
         print("\n*** RESULTADO ***\n")
         if(C==1 and D==1 and U==1):
-        print(num,"=",C,"centena,",D,"dezena e",U,"unidade.")
+            print(num,"=",C,"centena,",D,"dezena e",U,"unidade.")
         elif(C!=1 and D!=1 and U!=1):
-        print(num,"=",C,"centenas,",D,"dezenas e",U,"unidades.")
+            print(num,"=",C,"centenas,",D,"dezenas e",U,"unidades.")
         elif(C==1 and D==1 and U!=1):
-        print(num,"=",C,"centena,",D,"dezena e",U,"unidades.")
+            print(num,"=",C,"centena,",D,"dezena e",U,"unidades.")
         elif(C==1 and D!=1 and U==1):
-        print(num,"=",C,"centena,",D,"dezenas e",U,"unidade.")
+            print(num,"=",C,"centena,",D,"dezenas e",U,"unidade.")
         elif(C!=1 and D==1 and U==1):
-        print(num,"=",C,"centenas,",D,"dezena e",U,"unidade.")
+            print(num,"=",C,"centenas,",D,"dezena e",U,"unidade.")
         elif(C==1 and D!=1 and U!=1):
-        print(num,"=",C,"centena,",D,"dezenas e",U,"unidades.")
+            print(num,"=",C,"centena,",D,"dezenas e",U,"unidades.")
         elif(C!=1 and D!=1 and U==1):
-        print(num,"=",C,"centenas,",D,"dezenas e",U,"unidade.")
+            print(num,"=",C,"centenas,",D,"dezenas e",U,"unidade.")
         elif(C!=1 and D==1 and U!=1):
-        print(num,"=",C,"centenas,",D,"dezena e",U,"unidades.")
+            print(num,"=",C,"centenas,",D,"dezena e",U,"unidades.")
 except:
     print("\n*** VALOR INVÁLIDO: APENAS NÚMEROS INTEIROS! ***")
