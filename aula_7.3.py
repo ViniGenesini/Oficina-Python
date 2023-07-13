69c6943aac7f34adc68a136422d7ddf68c8e3f6a
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 21:19:00 -0300

    aula_7.3.py

--- aula_7.3.py
+++ aula_7.3.py
@@ -1,4 +1,17 @@
-# EXERCÍCIO 3
+''' 
+EXERCÍCIO 3
+Desenvolva um programa que leia peso e a altura de uma
+pessoa, calcule seu IMCe mostre seu status, de acordo 
+com a tabela abaixo:
+
+- Abaixo de 18.5: Abaixo do Peso
+- Entre 18.5 e 25: Peso Ideal
+- 25 até 30: Sobrepeso
+- 30 até 40: Obesidade
+- Acima de 40: Obesidade Mórbida
+(Obs: Utilize try except)
+
+'''
 
 try:
     print("*** CALCULADORA DE IMC *** \n")
@@ -14,6 +27,6 @@ try:
     elif(IMC>30 and IMC<=40):
         print("\n*** RESULTADO *** \n\nIMC =","%.2f"%IMC,"\nStatus: Obesidade")
     else:
-        print("\n*** RESULTADO *** \n\nIMC =","%.2f"%IMC,"\nStatus: Obesidade Mórbida")   
+        print("\n*** RESULTADO *** \n\nIMC =","%.2f"%IMC,"\nStatus: Obesidade Mórbida")
 except:
     print("*** VALOR(ES) INVÁLIDO(S)! ***")
