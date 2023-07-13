8ef552712545896ace58ad4f2a408f7d8aa6ede1
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 21:40:29 -0300

    aula_7.7.py

--- aula_7.7.py
+++ aula_7.7.py
@@ -1,4 +1,16 @@
-# EXERCÍCIO 7
+''' 
+EXERCÍCIO 7
+Escreva um programa que leia um número n inteiro
+qualquer e mostrena tela os n primeiros elementos
+de uma Sequência de Fibonacci. (“Fibonacci é uma 
+sequência de números, que aparece em certos mistérios
+da natureza e da vida, onde a sequência inicia com 0
+e 1, e os números seguintes serão a soma dos dois
+números anterior”.)
+
+Ex: 0 → 1 → 1 → 2 → 3 → 5 → 8
+
+'''
 
 try:
     print("*** SEQUÊNCIA DE FIBONACCI ***\n")
