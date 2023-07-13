4e242b56c94dc13e4cd4b280fed59543b5749447
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 21:47:04 -0300

    aula_7.9.py

--- aula_7.9.py
+++ aula_7.9.py
@@ -1,4 +1,12 @@
-# EXERCÍCIO 9
+''' 
+EXERCÍCIO 9
+Desenvolva um programa que leia vários números inteiros
+pelo teclado. No final da execução, mostre a média entretodos
+os valores e qual foi o maior e o menor valores lidos. O
+programa deve perguntar ao usuário se elequer ou não continuar
+a digitar valores.
+
+'''
 
 try:
     print("*** NÚMEROS INTEIROS 2 ***\n")
@@ -9,12 +17,12 @@ try:
         num.append(x)
         resposta = input("Deseja continuar, sim ou não? (S / N): ")
         if resposta=='N':
-        print("\n*** RESULTADO ***\n")
-        print("Média dos números:",sum(num)/len(num))
-        num.sort()
-        print("Maior número:",num[-1])
-        print("Menor número:",num[0])
+            print("\n*** RESULTADO ***\n")
+            print("Média dos números:",sum(num)/len(num))
+            num.sort()
+            print("Maior número:",num[-1])
+            print("Menor número:",num[0])
         elif resposta!='S':
-        print("\n*** RESPOSTA INVÁLIDA! ***")
+            print("\n*** RESPOSTA INVÁLIDA! ***")
 except:
     print("\n*** VALOR INVÁLIDO: APENAS NÚMEROS INTEIROS! ***")
