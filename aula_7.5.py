38912d5f80a62eaee0aa34e91ea6565ccef7577d
ViniGenesini <102668303+ViniGenesini@users.noreply.github.com>
2023-07-12 21:28:35 -0300

    aula_7.5.py

--- aula_7.5.py
+++ aula_7.5.py
@@ -1,4 +1,20 @@
-# EXERCÍCIO 5
+''' 
+EXERCÍCIO 5
+Faça um Programa para um caixa eletrônico. O programa deverá
+perguntar ao usuário a valor do saque e depois informar 
+quantas notas de cada valor serão fornecidas. As notas 
+disponíveis serão as de 1, 5, 10, 50 e 200 reais. O valor 
+mínimo é de 20 reais e o máximo de 700 reais. O programa não 
+deve se preocupar com a quantidade de notas existentes na
+máquina. 
+
+Exemplo 1: Para sacar a quantia de 256 reais, o programa 
+fornece uma nota de 200, uma nota de 50, uma nota de 5 e uma
+nota de 1;
+Exemplo 2: Para sacar aquantia de 371 reais, o programa fornece
+duas notas de 200, duas notas de 50, setenotas de 10 e uma de 1.
+
+'''
 
 print("*** CAIXA ELETRÔNICO ***\n\n")
 try:
